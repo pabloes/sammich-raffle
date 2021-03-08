6925d167c49e3cc9bc884e2277b4bcb0209bec83
Pablo Estornut <estornut@gmail.com>
2021-03-08 13:27:00 +0100

    rarible supporters

---
+++ supporters/rarible-pablo.js
@@ -0,0 +1,58 @@
+const owners = {
+    "0x3e00ffa2fa519b0cb592f9fa2382711b3e36fb8f":{//redgirl 20years
+        address:"0x3e00ffa2fa519b0cb592f9fa2382711b3e36fb8f",
+        score:1
+    },
+    "0x7ed50367790edcf41592e17090b2f16d48d0ea47":{//doll+bro
+        address:"0x7ed50367790edcf41592e17090b2f16d48d0ea47",
+        score:2
+    },
+    "0xd387a6e4e84a6c86bd90c158c6028a58cc8ac459":{//doll+bro pranksy
+        address:"0xd387a6e4e84a6c86bd90c158c6028a58cc8ac459",
+        score:18
+    },
+    "0xd387a6e4e84a6c86bd90c158c6028a58cc8ac459":{//eth.picture YfiFan
+        address:"0xedba5d56d0147aee8a227d284bcaac03b4a87ed4",
+        score:1
+    },
+    "0xbe3fbebc1cc29e99e02237dac84860e268cabc9b":{//grandma
+        address:"0xbe3fbebc1cc29e99e02237dac84860e268cabc9b",
+        score:1
+    },
+    "0xcd0b3ad54ff89f5aaf3497874b9f18a91b0889ae":{//grandma+frog powersurge
+        address:"0xcd0b3ad54ff89f5aaf3497874b9f18a91b0889ae",
+        score:2
+    },
+    "0x5b9f96ba92fcf03d4bb22dcb658a07f702008e52":{//grandma&frog urosaki
+        address:"0x5b9f96ba92fcf03d4bb22dcb658a07f702008e52",
+        score:2
+    },
+    "0x84300dcc7ca9cf447e886fa17c11fa22557d1af0":{//frog Rutger
+        address:"0x84300dcc7ca9cf447e886fa17c11fa22557d1af0",
+        score:1
+    },
+    "0x19d97534a01fcfb68a9aa28d694dd07d2229d397":{//frog 
+        address:"0x19d97534a01fcfb68a9aa28d694dd07d2229d397",
+        score:1
+    },
+    "0x16893135b0621bbac41920187d607c7d61bf422c":{//rudolph 
+        address:"0x16893135b0621bbac41920187d607c7d61bf422c",
+        score:1
+    },
+    "0x99cbb4509625e621e0d614b2947276b0ff0fa6d4":{//frog majorart 
+        address:"0x99cbb4509625e621e0d614b2947276b0ff0fa6d4",
+        score:1
+    },
+    "0xbe3fbebc1cc29e99e02237dac84860e268cabc9b":{//who1
+        address:"0xbe3fbebc1cc29e99e02237dac84860e268cabc9b",
+        score:1
+    },
+    "0x007f44362400de9f364efa919ff84c6ea9e210d5":{//who2 Proteu5
+        address:"0x007f44362400de9f364efa919ff84c6ea9e210d5",
+        score:1
+    }
+};
+
+console.log(owners);
+
+module.exports = owners;
