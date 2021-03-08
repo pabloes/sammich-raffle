159aeca58f4824c76c65b660583c283e1cd58961
Pablo Estornut <estornut@gmail.com>
2021-03-08 13:47:15 +0100

    clean comments

--- index2.js
+++ index2.js
@@ -15,25 +15,3 @@ if(!myArgs[0]) {
         return `${prizes[index]}: ${name}`;
     }).join('\n'));
 })();
-
-/* 
-const test = {};
-
-let times = 999;
-
-
-(async () => {
-    const logsData = await axios.get("https://mana-fever.fun/logs-json").then((response)=>response.data);
-    while(times--){
-        const winners = getRaffleResult(Number.MAX_SAFE_INTEGER-times, 6, logsData);
-      // console.log(winners)
-        winners.forEach((name)=>{
-            test[name] = test[name] || 0;
-            test[name]++;
-        })
-    }    
-    
-    const sorted = Object.keys(test).map((name)=>({name, times:test[name]})).sort((a,b)=>b.times-a.times);
-
-    console.log(sorted.map(s=>`${s.name} ${s.times}`).join('\n'));
-})(); */
